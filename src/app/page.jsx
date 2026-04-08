045d85658fe9d93121b4c5921ac01c069a9d42d6
Jeisson Benitez <jeisson.benitez213@pascualbravo.edu.co>
2026-04-08 01:26:46 -0500

    first commit, initial versión of header

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const page = () => {
+  return (
+    <>
+      <main className="w-full h-screen flex justify-center items-center">
+        <p>data</p>
+      </main>
+    </>
+  );
+};
+
+export default page;
